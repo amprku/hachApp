8441bbf9725cc626bc2cf31491c60d8e84a6e4d0
uhmeat <amitkulkarni43@gmail.com>
2016-05-31 21:46:20 -0700

    Add files via upload
    Inital commit - blank protoype

---
+++ hachApp/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  hachApp
+//
+//  Created by Amit Kulkarni on 5/31/16.
+//  Copyright (c) 2016 Amit Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
